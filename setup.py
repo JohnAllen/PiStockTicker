3890eb11eac9297b765e73d82fad839ef6824e6b
John Allen <johnjalleniii@gmail.com>
2016-02-11 13:22:34 -0800

    correct email

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
   version = '0.1',
   description = "Print a stock\'s price volume and percentage every five seconds using a Raspberry Pi",
   author = 'JohnAllen',
-  author_email = 'johnnjalleniii@gmail.com',
+  author_email = 'johnjalleniii@gmail.com',
   url = 'https://github.com/JohnAllen/PiStockTicker',
   download_url = 'https://github.com/JohnAllen/PiStockTicker/tarball/0.1',
   dependency_links=['https://github.com/adafruit/Adafruit-Raspberry-Pi-Python-Code/tree/master/Adafruit_CharLCD'],
